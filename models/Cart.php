282026b5b930d187bbdf60100580c9af16329ba5
Jorge <lassancejorgeluiz@gmail.com>
2019-06-18 04:16:48 -0300

    Finalizando frete, PCBIA

--- models/Cart.php
+++ models/Cart.php
@@ -17,7 +17,12 @@ class Cart extends Model{
 				'qt' => $qt,
 				'price' => $info['price'],
 				'name' => $info['name'],
-				'image' => $info['image']
+				'image' => $info['image'],
+				'weight' => $info['weight'],
+				'width' => $info['width'],
+				'height' => $info ['height'],
+				'length' => $info['length'],
+				'diameter' => $info['diameter']
 			);
 		}
 
@@ -40,6 +45,35 @@ class Cart extends Model{
 
 		);
 
+		$list = $this->getList();
+		$nVlPeso = 0;
+		$nVlComprimento = 0;
+		$nVlAltura = 0;
+		$nVlLargura = 0;
+		$nVlDiametro = 0;		
+		$nVlValorDeclarado = 0;
+		foreach($list as $item){
+			$nVlPeso += floatval($item['weight']);
+			$nVlComprimento += floatval($item['length']);
+			$nVlAltura += floatval($item['height']);
+			$nVlLargura += floatval($item['width']);
+			$nVlDiametro += floatval($item['diameter']);
+			$nVlValorDeclarado += floatval($item['price'] * $item['qt']); 
+		}
+
+		$soma = $nVlComprimento + $nVlAltura + $nVlLargura;
+		if($soma > 200){
+			$nVlComprimento = 66;
+			$nVlAltura = 66;
+			$nVlLargura = 66;
+		}
+		if($nVlDiametro > 90){
+			$nVlDiametro = 90;
+		}
+		if($nVlPeso > 40){
+			$nVlPeso = 40;
+		}
+
 		global $config;
 
 		$data = array(
@@ -47,15 +81,15 @@ class Cart extends Model{
 			'nCdServico' => '40010',
 			'sCepOrigem' => $config['cep_origin'],
 			'sCepDestino' => $cepDestination,
-			'nVlPeso' => '',
+			'nVlPeso' => $nVlPeso,
 			//Caixa 1, envolope 3
 			'nCdFormato' => '1',
-			'nVlComprimento' => '',
-			'nVlAltura' => '',
-			'nVlLargura' => '',
-			'nVlDiametro' => '',
+			'nVlComprimento' => $nVlComprimento,
+			'nVlAltura' => $nVlAltura,
+			'nVlLargura' => $nVlLargura,
+			'nVlDiametro' => $nVlDiametro,
 			'sCdMaoPropria' => 'N',
-			'nVlValorDeclarado' => '',
+			'nVlValorDeclarado' => $nVlValorDeclarado,
 			'sCdAvisoRecebimento' => 'N',
 			'StrRetorno' => 'xml'
 		);
@@ -68,8 +102,8 @@ class Cart extends Model{
 		$r = curl_exec($ch);
 		$r = simplexml_load_string($r);
 
-		$array['price'] = $r->cServico->Valor;
-		$array['date'] = $r->cServico->PrazoEntrega;
+		$array['price'] = current($r->cServico->Valor);
+		$array['date'] = current($r->cServico->PrazoEntrega);
 
 		return $array;
 	}
